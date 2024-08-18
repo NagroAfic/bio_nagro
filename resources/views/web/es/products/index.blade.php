406a554e06b196ee613b62e0bea256f4d4077865
Jose Saldaña <nagrogamesafic@gmail.com>
2024-08-18 11:15:55 -0500

    Update index.blade.php

--- resources/views/web/es/products/index.blade.php
+++ resources/views/web/es/products/index.blade.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container mt-5">
     <div class="row">
         <div class="col-12 d-flex justify-content-center align-items-center">
             <!-- Imagen del producto solo 1 -->
