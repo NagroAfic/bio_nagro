a267b76d6c0e2c551007d3ed8b8acda3ca9a60b5
Jose Saldaña <nagrogamesafic@gmail.com>
2024-08-18 11:50:34 -0500

    Update product.blade.php

--- resources/views/web/product.blade.php
+++ resources/views/web/product.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     @if($page_traduction == "ES")
         @include('web.es.products.index')
+        @include('web.es.form')
     @else
         @include('web.en.product.index')
     @endif
