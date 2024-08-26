ea944a16aee905ea00a6d1a98be92dbc9914ed40
Jose Saldaña <josesaldanavi@gmail.com>
2024-08-26 17:16:58 -0500

    update

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -19,9 +19,16 @@
 @section('script')
 <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
 <script>
+    
+    @if($page_traduction == "ES")
+        let texto = ['Fiabilidad y excelencia en cada uno de nuestros productos','Cada producto refleja nuestro compromiso con la calidad','Diseño robusto y precisión en cada detalle','Calidad inigualable que puedes confiar en cada uso']
+    @else
+        let texto = ['Reliability and excellence in each of our products', 'Each product reflects our commitment to quality', 'Robust design and precision in every detail', 'Unmatched quality that you can trust in every use']
+    @endif
+
     var typed = new Typed('#element', {
-      strings: ['Calidad y precisión entregada en cada producto'],
-      typeSpeed: 30,
+      strings: texto,
+      typeSpeed: 15,
       loop: true,
       loopCount: Infinity,
     });
