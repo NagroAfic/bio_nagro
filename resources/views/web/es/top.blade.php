d994a7a3630fa619e8e2dcb4e3878a1705e8b14f
Jose Saldaña <nagrogamesafic@gmail.com>
2024-11-16 01:50:33 -0500

    cambio en animacion

--- resources/views/web/es/top.blade.php
+++ resources/views/web/es/top.blade.php
@@ -1,4 +1,4 @@
 <header class="header px-4">
-    <h1 class="text-center text-white">BIOSERVICECLAB</h1>
-    <p class="text-center text-white" id="element"></p>
+    <h1 class="text-center text-white wow animate__animated animate__fadeInUp">BIOSERVICECLAB</h1>
+    <p class="text-center text-white wow animate__animated animate__fadeInDown" id="element2">Calidad inigualable que puedes confiar en cada uso</p>
 </header>
