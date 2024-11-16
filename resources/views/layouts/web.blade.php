cd8ecce99a54290588b5cfdb9f656d0d5a3d2dc6
Jose Saldaña <nagrogamesafic@gmail.com>
2024-11-16 02:11:35 -0500

    update

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link rel="shortcut icon" href="{{ asset('images/ico_ave_bio.jpg') }}" type="image/x-icon">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
